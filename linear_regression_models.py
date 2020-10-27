db42f5e20955368b7e296eb43b6ac9807aff95f8
carmenpemsler <carmenpemsler@gmail.com>
2020-10-26 23:48:16 -0600

    Added my project

--- linear_regression_models.py
+++ linear_regression_models.py
@@ -5,7 +5,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import matplotlib
+import numpy as np
+matplotlib.use('TkAgg') #matplotlib backends - solves issue of SIGSEGV
 
 # import numpy as np
 
@@ -48,29 +50,104 @@ def drop_non_busdays(df, holidays=None):
 
 # FOR OAT VALUES
 OAT_busdays = (drop_non_busdays(OAT_dailyAvg, holidays=None))
-print(OAT_busdays)
+#print(OAT_busdays)
 # FOR ELEC USE VALUES
 ElecUse_busdays = (drop_non_busdays(ElecUse_dailyTotals, holidays=None))
-print(ElecUse_busdays)
+#print(ElecUse_busdays)
 
 # LOOKING FOR A CHANGE POINT THAT RESULTS IN OPTIMAL RMSE
 df00 = OAT_busdays.reset_index()
 df01 = ElecUse_busdays.reset_index()
 
+df_for_temps = pd.concat([df00, df01], axis=1)
+print('Data frame for FOR LOOP: ')
+print(df_for_temps)
+
 # max and min of reevaluated data
-min_temp = min(df00['OAT (F)'])
-max_temp = max(df00['OAT (F)'])
+#min_temp = int(round(min(df_for_temps['OAT (F)'])))
+#max_temp = int(round(max(df_for_temps['OAT (F)'])))
+
+#index_of_maxTemp = df_for_temps['OAT (F)'].idxmax()
+index_max= df_for_temps.loc[df_for_temps['OAT (F)'].idxmax()]
+index_min= df_for_temps.loc[df_for_temps['OAT (F)'].idxmin()]
 
 
-# Stepping through every increment of temperature
-def range_with_floats(start, stop, step):
-    while stop > start:
-        yield start
-        start += step
+
+#Stepping through every increment of temperature
+#def range_with_floats(start, stop, step):
+#    while stop > start:
+#        yield start
+#        start += step
 
 
-for i in range_with_floats(min_temp, max_temp, 1):
-    print(i)
+def frange(start, stop=None, step=None):
+    # if stop and step argument is None set start=0.0 and step = 1.0
+    start = float(start)
+    if stop == None:
+        stop = start + 0.0
+        start = 0.0
+    if step == None:
+        step = 1.0
+
+    count = 0
+    while True:
+        x = float(start + count * step)
+        if step > 0 and x >= stop:
+            break
+        elif step < 0 and x <= stop:
+            break
+        yield x
+        count += 1
+
+#results = np.empty(len(temps)
+
+
+import numpy
+#from sklearn import linear_model
+
+for temps in frange(index_min['OAT (F)'], index_max['OAT (F)'], 1.0):
+    temperatures=round(temps)
+    print(temperatures)
+
+
+#rounded_temp_df = round(df_for_temps['OAT (F)'])
+
+#for i in len((temperatures)):
+#    subset = rounded_temp_df[rounded_temp_df["OAT (F)"] > temperatures[i]]
+#    lin_reg = linear_model.LinearRegression()
+#    lin_reg.fit(subset["OAT (F)"], subset["Elec. Use (kWh)"])
+
+
+
+
+
+#temps = range(index_min, index_max, 1))
+#print('temperature range: ', temps)
+
+#for temps, df00 in temps:
+#    print(temps)
+#    print(df00)
+
+
+#from sklearn import linear_model
+
+#WORKING ON CODE HERE:_____________________________________________________________
+#results = np.empty(len(str(temps)))
+#from sklearn import linear_model
+
+#for i in range(len(temps))
+#    temperatures = temps[i]
+#    dftemps_float = df_for_temps.astype(int)
+#    subset = dftemps_float[dftemps_float["OAT (F)"] > temps(i)]
+#    lin_reg = linear_model.LinearRegression()
+#    lin_reg.fit(subset["OAT (F)"], subset["Elec. Use (kWh)"])
+
+#results[temps[i]] = temps[i]
+
+#results[rsme] = rsme from linear model
+
+
+
 
 #    Subset = data[temps > temps[i]]
 # busdays_data_subset = i[(i > 45)]
@@ -92,8 +169,6 @@ sns.regplot(x=OAT_dailyAvg, y=ElecUse_dailyTotals, color='red', marker='+')
 
 plt.xlabel('Outside Air Temperature (F)', size=18)
 plt.ylabel('Daily Elec. Use (kWh)', size=18)
-
-# plt.show()
 
 
 # printing the equation of the line
@@ -123,5 +198,3 @@ print('Subset of data: ')
 print(data_subset)
 
 #plt.show()
-
-
